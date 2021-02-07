f8a152302d0006709e457e606aa0714fbb13a3d9
aith <ariiramanesh@gmail.com>
2021-02-06 21:56:40 -0800

    built some worley noise base functions

--- my_world.js
+++ my_world.js
@@ -14,16 +14,6 @@
     p3_drawAfter
 */
 
-// todo
-// - add animals animals grazing, maybe they also cause ripples. im thinking we need a spawned array that checks if spawn like w placeTile
-// vegetation https://pablogamedev.itch.io/free-isometric-ature-voxel-enviroment
-// https://finalbossblues.itch.io/animals-sprite-pack
-// https://seliel-the-shaper.itch.io/monsterbattlerset
-// isometric items https://ssugmi.itch.io/16x16-pixel-kitchenwareingredients-pack
-// https://withering-systems.itch.io/city-game-tileset
-// viking ship https://helianthus-games.itch.io/pixel-art-viking-ship-16-directions
-//
-
 class Sprite {
   constructor(animation, option, speed) {
     this.animation = animation;
@@ -55,24 +45,11 @@ class Sprite {
   }
 }
 
-let tilesetImage;
-let animalImage;
-let lighthouseImage;
-let pigImage;
-let pigs = [];
-let pig;
 function p3_preload() {
-  tilesetImage = loadImage(
-    "https://cdn.glitch.com/c3daddd3-de7a-49cd-bbfa-e749378a8fbf%2Ftilesheet_complete.png?v=1612566704408"
-  ); // from Kenney.nl
-  animalImage = loadImage(
-    "https://cdn.glitch.com/c3daddd3-de7a-49cd-bbfa-e749378a8fbf%2Fanimals3.png?v=1612548365370"
-  );
-  lighthouseImage = loadImage("https://cdn.glitch.com/c3daddd3-de7a-49cd-bbfa-e749378a8fbf%2Flighthouse.png?v=1612566005585");
-  pig = new Sprite(animalImage, 5, 0.0007);
 }
 
-function p3_setup() {}
+function p3_setup() {
+}
 
 let worldSeed;
 
@@ -80,8 +57,6 @@ function p3_worldKeyChanged(key) {
   worldSeed = XXH.h32(key, 0);
   noiseSeed(worldSeed);
   randomSeed(worldSeed);
-
-  tileSprites = {};
 }
 
 function p3_tileWidth() {
@@ -105,77 +80,79 @@ function p3_tileClicked(i, j) {
 
 function p3_drawBefore() {}
 
+// Q: do i need to store this much data?
+let hasHome = {}, hasSeed = {}, hasCount = {}, hasPlotted = {}, foundClosest = {};
+let seeds = {}, evals = {}, counts = {}, distances = {};
 function p3_drawTile(i, j) {
-  noStroke();
-
   let key = [i, j];
-  if (!tileSprites[key]) {
-    // let subtypeSeed = worldSeed + i + "," + j;  // TODO is this correct?
-    let n = (noise(i, j) * 4) + 17 | 0;  // pick tile here
-    // let n = 18;
-    tileSprites[key] = n;
-  }
-
+  let feats;
+  if (!hasHome[key]) {      evals[key] = genEvalPoint(i, j); hasHome[key] = true; }
+  if (!hasSeed[key]) {      seeds[key] = getSeed(i, j); hasSeed[key] = true; }
+  if (!hasCount[key]) {     counts[key] = getPointCount(i, j, seeds[key]); hasCount[key] = true; }
+  if (!hasPlotted[key]) {   feats = genFeatPoints(i, j); hasPlotted[key] = true; }
+  if (!foundClosest[key]) { findClosestPointInside(i, j, feats); foundClosest[key] = true; }  // 5 n 666666
+  noStroke();
   if (XXH.h32("tile:" + [i, j], worldSeed) % 4 == 0) {
     fill(240, 200);
   } else {
     fill(255, 200);
   }
-
-  function r(i, j) {
-    let now = millis() / 1000.0;
-    let h = 1 * noise(i / 10 + 1.0 * now, j / 10 - 0.5 * now, now / 5);
-    for (let [ri, rj, t] of rippleSources) {
-      let di = i - ri;
-      let dj = j - rj;
-      let r = sqrt(di * di + dj * dj) + 1; // + 1 iot make tile with radius == 0 not be erased
-      let timePassed = abs(t - millis());
-      let easeOut = exp(-timePassed / 500.0);
-      h += 0.5 * ((sin(r - timePassed / 100.0) / r) * r) * easeOut;
-      if (timePassed > 5000) rippleSources.shift();
-    }
-    return h * p3_tileWidth() - 15;
-  }
   push(); // start new drawing state
   // beginShape();
-  // vertex(-tw, 0 - r(i, j));  // defined in .p3_tileHeight
   // vertex(0, th - r(i, j));
   // vertex(tw, 0 - r(i, j));
   // vertex(0, -th - r(i, j));
   // endShape(CLOSE);
   // let a = random(4) | 0;
-  let o = r(i, j);
-  placeTile(tileSprites[key], 0, o);
-
   let c = clicks[[i, j]] | 0;
   if (c % 2 == 1) {
-    translate(-20, -90);
-    image(lighthouseImage, 0, o/9);
-    // fill(0, 0, 0, 32);
-    // ellipse(0, 0, 10, 5); // how does it know to create it at the tile? push(), and prolly the engine.js draw()
-    // translate(0, -10);
-    // fill(255, 255, 100, 128);
-    // ellipse(0, 0, 10, 10); // shadow
+    translate(-20, -90)
   }
-
   pop();
 }
 
-function placeTile(ti, tj, hOff) {
-  // ti and tj determine tile used
-  image(tilesetImage, -32, -16 + hOff, 64, 64, ti * 111, tj * 128, 111, 128); // take offset from lookup(code)
+function getEvalPoint(i, j) {
+  noiseSeed([i, j] + "x");
+  let x = i + noise();
+  noiseSeed([i, j] + "y");
+  let y = j + noise();
+  return [x, y];
 }
 
-function p3_drawAnimal(i, j) {
-  if (false) {
-    push();
-    let tr = 15;
-    translate(tr, 0);
-    pig.show();
-    pig.animate();
-    translate(-tr, 0);
-    pop();
+function getSeed(i, j) {
+  return XXH.h32("tile:" + [i, j]);
+}
+
+function getPointCount(i, j, seed) {
+  noiseSeed(seed);
+  let max = 4;
+  let count = constrain(noise(1,max+1), 1, max) | 0;  // how dould I have done poisson?
+  return count;
+}
+function genFeatPoints(i, j, count) {
+  noiseSeed(seed);
+  let feats = [];
+  for(let p=0; p<count; p++) {
+    feats.push([i + noise(), j + noise()]);
   }
+  if (i == 0 && j == 1) print(feats);
+  return feats;
+}
+function findClosestPointInside(i, j, points) {
+  let ld=dist(i, j, points[0][0], points[0][1]);
+  let cp=0;
+  for(let p=1; p<points.length; p++) {
+    let d = dist(i, j, points[p][0], points[p][1]);
+    if (d < ld) {
+      ld = d;
+      cp = p;
+    }
+  }
+  return points[cp];
+}
+
+function placeTile(ti, tj, hOff) {
+  image(tilesetImage, -32, -16 + hOff, 64, 64, ti * 111, tj * 128, 111, 128); // take offset from lookup(code)
 }
 
 function p3_drawSelectedTile(i, j) {
